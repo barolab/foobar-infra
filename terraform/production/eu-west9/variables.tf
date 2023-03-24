c1ce8254bd888d31dd2cc6c0bb9caf4ebdc0f703
barolab <rbailly17000@gmail.com>
2023-03-24 14:49:11 +0100

    feat(): add foobar-api application (#4)
    Add a copy of the foobar-api into this repository so we can easily
    release a Docker image for it.
    The release is on all branches for now, and will push the image to
    ghcr.io

--- terraform/production/eu-west9/variables.tf
+++ terraform/production/eu-west9/variables.tf
@@ -3,11 +3,33 @@ variable "project" {
   type        = string
 }
 
+variable "ghcr_user" {
+  description = "The username of the GHCR user who will pull artefacts"
+  type        = string
+}
+
+variable "ghcr_email" {
+  description = "The email of the GHCR user who will pull artefacts"
+  type        = string
+}
+
+variable "ghcr_password" {
+  description = "The password of the GHCR user who will pull artefacts"
+  type        = string
+}
+
 locals {
   region = "europe-west9"
   zones  = ["europe-west9-a", "europe-west9-b", "europe-west9-c"]
 
   cluster = {
     name = "foobar-${local.region}"
+  }
+
+  namespaces = {
+    foobar             = {}
+    kube-network       = {}
+    kube-security      = {}
+    kube-observability = {}
   }
 }
