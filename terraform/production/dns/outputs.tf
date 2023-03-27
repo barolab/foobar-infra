90d0f557754c488dfd62f8da2590be610aa01e98
Bailly Romain <rbailly17000@gmail.com>
2023-03-27 11:16:48 +0200

    feat(): load balancing at DNS level

--- terraform/production/dns/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "eu_lb" {
-  description = "Details about the EU Load Balancer"
-  value = data.kubernetes_service.traefik_proxy_eu
-}
-
-output "us_lb" {
-  description = "Details about the US Load Balancer"
-  value = data.kubernetes_service.traefik_proxy_us
-}
